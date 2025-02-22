93ff0f0976b7619a7ae8179a239ac00d5faef5bd
satyaprasadjena1 <satyaprasadjena1@gmail.com>
2025-02-22 20:31:30 +0530

    Updated message in index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({extended: true}))
 app.use("/uploads", express.static("uploads"))
 
 app.get('/', function(req, res){
-  res.json({message: "Hello chai aur code"})
+  res.json({message: "Hello Shibaprasad"})
 })
 
 app.post("/upload", upload.single('file'), function(req, res){
